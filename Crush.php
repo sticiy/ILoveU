ec3830ebf1b4d24eac203bbe9bfca08f0a9ea6e9
sticiy <118414038+sticiy@users.noreply.github.com>
2025-12-16 03:56:09 +0700

    Fix grammar

--- Crush.php
+++ Crush.php
@@ -12,7 +12,7 @@ class Crush {
 
 class getIloveYou extends Crush{
 	public function IloveYou($name, $writer){
-		return "\nI don't know why i still loving at you, I know it's crazy but yeah. . . {$name}. \nHope you will never having bad nature like me, because \nyou're not type of person who controled by media trend like having boyfriend for their life" . PHP_EOL. "\n\nWriter : {$writer}\n\n";
+		return "\nI don't know why I still love you, I know it's crazy but yeah. . . {$name}. \nHope you will never have bad nature like me, because \nyou're not type of person who controlled by media trends like having boyfriend." . PHP_EOL. "\n\nWriter : {$writer}\n\n";
 	}
 }
 
